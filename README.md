ce331ae9485a01258ccabce2f8d663843d63f1b3
annikagomez <45185380+annikagomez@users.noreply.github.com>
2025-08-12 11:09:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# viral_metaT_pipeline
---
+++ snakemake.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=snakemake
+#SBATCH --partition=RM-shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=3-00:00:00
+
+source /jet/home/agomez3/.bashrc
+source activate /jet/home/agomez3/miniforge3/envs/snakemake
+
+snakemake --unlock
+snakemake --use-conda --rerun-incomplete --workflow-profile profile/
