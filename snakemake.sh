aadda071bf4856f2a35f05ec0056876ff00ab7d9
annikagomez <45185380+annikagomez@users.noreply.github.com>
2025-08-12 11:17:22 -0400

    Update snakemake.sh

--- snakemake.sh
+++ snakemake.sh
@@ -5,8 +5,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=3-00:00:00
 
-source /jet/home/agomez3/.bashrc
-source activate /jet/home/agomez3/miniforge3/envs/snakemake
+###ADJUST FOLLOWING TWO LINES TO ACTIVATE YOUR SNAKEMAKE ENVIRONMENT###
+###source /jet/home/agomez3/.bashrc
+###source activate /jet/home/agomez3/miniforge3/envs/snakemake
 
 snakemake --unlock
 snakemake --use-conda --rerun-incomplete --workflow-profile profile/
